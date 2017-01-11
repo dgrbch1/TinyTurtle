ebd9a854f5cb642bd34996a14b7650b369db237b
Christopher Gonzalez <wqweqweas@gmail.com>
2017-01-11 22:13:26 +0000

    house

---
+++ script.js
@@ -0,0 +1,26 @@
+   TinyTurtle.apply(window);
+  // stamp();
+   forward(40);
+   right(90);
+   forward(40);
+   right(90);
+   forward(40);
+   right(90);
+   forward(40);
+   
+  stamp();
+  forward(40);
+  right(90);
+  forward(40);
+  right(90);
+ forward(40);
+ right(90);
+ forward(40);
+ 
+stamp();
+right(136);
+forward(55);
+right(98);
+forward(50);
+right(74);
+forward(50);
